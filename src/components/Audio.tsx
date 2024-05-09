692b1ec13f6a854401608a1485962b7496c3df67
PabloMur <polillomurillo@gmail.com>
2024-05-09 13:08:42 -0300

    first

---
+++ src/components/Audio.tsx
@@ -0,0 +1,7 @@
+import audio from "../../public/audio.wav";
+
+const Audio = () => {
+  return <audio src={audio}></audio>;
+};
+
+export { Audio };
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <h2>Prueba técnica - Pablo Murillo</h2>
+      <h2>DEZURE</h2>
+    </header>
+  );
+};
+
+export { Header };
